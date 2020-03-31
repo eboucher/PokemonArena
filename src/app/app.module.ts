bf6fb953a23836bbb49623d5675ff5d4af1e87fa
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-03-31 14:38:05 +0200

    FIX app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LikeButtonComponent } from './like-button/like-button.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import { BattleComponent } from './battle/battle.component';
+import { AttackComponent } from './attack/attack.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LikeButtonComponent
+    LikeButtonComponent,
+    PokemonComponent,
+    BattleComponent,
+    AttackComponent
   ],
   imports: [
     BrowserModule
