ad4d5cccd469cc10ca455f487f092ac66223bd4e
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-04-02 15:06:15 +0200

    ADD tests logger service

--- src/app/service/logger.service.spec.ts
+++ src/app/service/logger.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LoggerService } from './logger.service';
+import LogType from '../model/logtype';
 
 describe('LoggerService', () => {
   let service: LoggerService;
@@ -12,5 +13,55 @@ describe('LoggerService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add a normal message', () => {
+    service.addNormalLog('hello world !');
+    expect(service.logs).toHaveLength(1);
+    expect(service.logs[0].type).toBe(LogType.Normal);
+    expect(service.logs[0].message).toBe('hello world !');
+  });
+
+  it('should add a pokemon1 message', () => {
+    service.addPokemon1Log('hello world !');
+    expect(service.logs).toHaveLength(1);
+    expect(service.logs[0].type).toBe(LogType.Pokemon1);
+    expect(service.logs[0].message).toBe('hello world !');
+  });
+
+  it('should add a pokemon2 message', () => {
+    service.addPokemon2Log('hello world !');
+    expect(service.logs).toHaveLength(1);
+    expect(service.logs[0].type).toBe(LogType.Pokemon2);
+    expect(service.logs[0].message).toBe('hello world !');
+  });
+
+  it('should add a dead message', () => {
+    service.addDeadLog('hello world !');
+    expect(service.logs).toHaveLength(1);
+    expect(service.logs[0].type).toBe(LogType.Dead);
+    expect(service.logs[0].message).toBe('hello world !');
+  });
+
+  it('should add a begin message', () => {
+    service.addBeginLog('hello world !');
+    expect(service.logs).toHaveLength(1);
+    expect(service.logs[0].type).toBe(LogType.Begin);
+    expect(service.logs[0].message).toBe('hello world !');
+  });
+
+  it('should add a damage message', () => {
+    service.addDamageLog("pikachu", 30);
+    expect(service.logs).toHaveLength(1);
+    expect(service.logs[0].type).toBe(LogType.Damage);
+    expect(service.logs[0].message).toBe('');
+    expect(service.logs[0].damage).toBe(30);
+    expect(service.logs[0].defender).toBe("pikachu");
+  });
+
+  it('should clear the logs array', () => {
+    service.addNormalLog('hello world');
+    service.clear();
+    expect(service.logs).toHaveLength(0);
   });
 });
