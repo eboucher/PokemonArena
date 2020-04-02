1bb205f37bdffafcca76aaa5df3eea1d1dade9bd
Bartholome <bartholome.lebruno@gmail.com>
2020-04-02 02:09:05 +0200

    correct test with directive

--- src/app/message/message.component.spec.ts
+++ src/app/message/message.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MessageComponent } from './message.component';
+import {ColorMessageDirective} from '../directive/color-message.directive';
 
 describe('MessageComponent', () => {
   let component: MessageComponent;
@@ -8,7 +9,7 @@ describe('MessageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MessageComponent ]
+      declarations: [ MessageComponent, ColorMessageDirective ],
     })
     .compileComponents();
   }));
