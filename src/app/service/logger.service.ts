ca2ab59f4a9939a180c729b36445daf9a0625a77
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-04-02 16:39:45 +0200

    ADD console.log to logger service

--- src/app/service/logger.service.ts
+++ src/app/service/logger.service.ts
@@ -13,22 +13,27 @@ export class LoggerService {
 
   addPokemon1Log(message: string): void {
     this.logs.push(new Log(LogType.Pokemon1, message));
+    console.log(message);
   }
 
   addPokemon2Log(message: string): void {
     this.logs.push(new Log(LogType.Pokemon2, message));
+    console.log(message);
   }
 
   addNormalLog(message: string): void {
     this.logs.push(new Log(LogType.Normal, message));
+    console.log(message);
   }
 
   addDeadLog(message: string): void {
     this.logs.push(new Log(LogType.Dead, message));
+    console.log(message);
   }
 
   addBeginLog(message: string): void {
     this.logs.push(new Log(LogType.Begin, message));
+    console.log(message);
   }
 
   addDamageLog(defender: string, damage: number): void {
@@ -36,6 +41,7 @@ export class LoggerService {
     logDamage.defender = defender;
     logDamage.damage = damage;
     this.logs.push(logDamage);
+    console.log(`${defender} prends ${damage} dégâts`);
   }
 
   clear(): void {
