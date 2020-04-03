79db2ae63d546a4d3a4ec834372c263c60615ba0
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-04-03 16:40:57 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LikeButtonComponent } from './like-button/like-button.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { BattleComponent } from './battle/battle.component';
-import { AttackComponent } from './attack/attack.component';
 import { MessageComponent } from './message/message.component';
 import { HomeComponent } from './home/home.component';
 import { ColorMessageDirective } from './directive/color-message.directive';
@@ -20,10 +18,8 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    LikeButtonComponent,
     PokemonComponent,
     BattleComponent,
-    AttackComponent,
     MessageComponent,
     HomeComponent,
     ColorMessageDirective
